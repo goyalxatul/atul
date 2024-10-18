bf462ce2605bde15aebce47e47e6463c8ffbdaad
goyalxatul <atulxgoyal@gmail.com>
2024-10-18 18:52:50 +0530

    second push

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes and Route
 import Navbar from './components/Navbar';
+import Skills from './components/Skills';
 
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Routes>
+      <Route path="/skills" element={<Skills />} />
+      </Routes>
     </Router>
   );
 }
